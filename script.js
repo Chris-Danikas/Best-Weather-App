e6fa767c36d4379ebf49cecc0356416eed283f4a
Chris-Danikas <christosda14@gmail.com>
2019-05-28 23:32:22 +0300

    just a temperature and a description

---
+++ script.js
@@ -0,0 +1,18 @@
+const URL = "https://api.openweathermap.org/data/2.5/weather?id=252848&APPID=7d8a1c597d7b9d3b30b5e42ef9fb621c&units=metric";
+let temp = document.getElementById("tempElem");
+let desc = document.getElementById("desc");
+
+window.addEventListener('load', () => {
+    getTemp(URL);    
+});
+
+
+function getTemp(url){
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        temp.innerHTML = data.main.temp + " °C";
+        desc.innerHTML = data.weather[0].description;
+    });
+}
