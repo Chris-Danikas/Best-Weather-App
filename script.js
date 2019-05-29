0e9e8092280d2501c2099a0aff1e9e35b9f00cbf
Chris-Danikas <christosda14@gmail.com>
2019-05-29 14:26:39 +0300

    doing it

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const URL = "https://api.openweathermap.org/data/2.5/weather?id=252848&APPID=7d8a1c597d7b9d3b30b5e42ef9fb621c&units=metric";
+const hourlyURL = "https://api.openweathermap.org/data/2.5/forecast?id=252848&APPID=7d8a1c597d7b9d3b30b5e42ef9fb621c&units=metric";
+//I can also use &lang=el
+
 let temp = document.getElementById("tempElem");
 let desc = document.getElementById("desc");
 
@@ -14,5 +17,10 @@ function getTemp(url){
         console.log(data);
         temp.innerHTML = Math.round(data.main.temp) + " °C";
         desc.innerHTML = data.weather[0].description;
-    });
+        return fetch(hourlyURL);
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
 }
